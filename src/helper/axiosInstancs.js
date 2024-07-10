2015aebe5a5c95ddc7d3615e6dae06a111c5af9a
sourabhsjadhav99 <sourabhsjadhav99@gmail.com>
2024-07-11 02:38:30 +0530

    url updated

--- src/helper/axiosInstancs.js
+++ src/helper/axiosInstancs.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 const token = sessionStorage.getItem('token');
 
 export const axiosInstance = axios.create({
-  // baseURL: 'https://entertainment-app-capstone-server.onrender.com/api',
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://entertainment-app-capstone-server.onrender.com/api',
+  // baseURL: 'http://localhost:8000/api',
   headers: {
     Authorization: token ? `Bearer ${token}` : '', // Include token in Authorization header if it exists
   },
